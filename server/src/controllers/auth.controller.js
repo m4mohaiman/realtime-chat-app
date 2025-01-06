f6e26e7458e03a5cbefff291267479bfc7c2a413
Mohaiman <m4mohaiman@gmail.com>
2025-01-06 17:38:45 +0600

    login , logout api create

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -5,6 +5,9 @@ import { generateToken } from "../lib/utils.js";
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
   try {
+    if (!email || !fullName || !password) {
+      return res.status(400).send("All fields are required.");
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -20,14 +23,12 @@ export const signup = async (req, res) => {
       //generate token
       generateToken(newUser._id, res);
       await newUser.save();
-      res
-        .status(201)
-        .json({
-          _id: newUser._id,
-          fullName: newUser.fullName,
-          email: newUser.email,
-          message: "User created successfully.",
-        });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        message: "User created successfully.",
+      });
     } else {
       console.log("Something went wrong.", error.message);
       res.status(400).json({ message: "Something went wrong." });
@@ -37,10 +38,34 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found." });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials." });
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+      profilePic: user.profilePic,
+      message: "Login successful.",
+    });
+  } catch (error) {
+    console.log("Error logging in:", error.message);
+    res.status(500).json({ message: "Internal server error." });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout successful." });
+  } catch (error) {
+    console.log("Error logging out:", error.message);
+    res.status(500).json({ message: "Internal server error." });
+  }
 };
