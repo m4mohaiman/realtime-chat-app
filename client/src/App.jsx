0df8c2354f83f5d555b8ef26bba90b48c2a64ca7
Mohaiman <m4mohaiman@gmail.com>
2025-01-07 17:52:57 +0600

    sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth , isCheckingAuth } = useAuthStore();
@@ -34,6 +35,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+      <Toaster/>
     </div>
   );
 };
